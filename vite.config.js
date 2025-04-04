df0448dde4c0daf0cdb64b3e984e9c2e311abe21
Yash-yadav2 <yashyadav20023@gmail.com>
2025-04-04 13:11:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "0.0.0.0",  // Allow access from all IPs
-    port: 5173,       // Ensure correct port
-    strictPort: true, // Ensures Vite doesn't switch ports
-    allowedHosts: ["casiḅom820.com", "www.casiḅom820.com"], // Add allowed domains
-    cors: {
-      origin: ["http://casiḅom820.com", "http://www.casiḅom820.com"], // Allow frontend domain
-      credentials: true, // Allow cookies & authentication headers
-    },
-  },
 });
